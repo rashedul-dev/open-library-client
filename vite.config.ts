958b81ca8c089ed6f7e5e8f8573d42c063f6106a
unknown <rashedulislam.edge@gmail.com>
2025-07-06 23:38:52 +0600

    Fix some bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
-
-// https://vite.dev/config/
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: './', // 🔥 this line is essential for correct routing and assets
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
