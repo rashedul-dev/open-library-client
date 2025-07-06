0a0cf6e08781cd5c39f382a18217539c13e6a77c
unknown <rashedulislam.edge@gmail.com>
2025-07-06 23:15:59 +0600

    Fix baseAPI domain

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,8 +4,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://lmsbackendapi.vercel.app/api/",
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://lmsbackendapi.vercel.app/api/",
+    // baseUrl: "http://localhost:5000/api",
   }),
   tagTypes: ["book", "borrow"],
   endpoints: (builder) => ({
